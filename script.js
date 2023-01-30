a22d618432cb77ba1ac7e6698edd1223ef0c5e87
jenyo21j <111152632+jenyo21j@users.noreply.github.com>
2023-01-30 15:32:17 +0800

    added js code

--- script.js
+++ script.js
@@ -1,137 +1,146 @@
 // setting up a 60 second countdown timer
 
-var count =45;
-var counter =setInterval(timer, 1000);
+var playOpt = ["Yes", "No"];
+    
+var restartBtn = document.getElementById("restart");
+var nextBtn = document.getElementById("next");
+var submitBtn = document.getElementById("submit");
+var trueBtn = document.getElementById("true");
+var falseBtn = document.getElementById("false");
+var userScore = document.getElementById("userScore");
+var totalScore = document.getElementById("totalScore");
+var questionText = document.getElementById("questionText")
 
-function timer(){
-  count=count-1;
-  if (count <= 0){
-     clearInterval(counter);
-     return;
-  }
-document.getElementById("timer").innerHTML="Timer: "+ count + " secs"; 
-}
-
-// generate js Qs
-    // create js questions
+var currentQ = 0;
+var score = 0;
 
 var myQuestions = [
     {
         question: "The Const keyword in Javascript can be redeclared",
-        answers:{
-            true: "yes",
-            false: "no",
-         },
-        correctAnswer: false 
+        answers:[
+            {option: "True", answer: false},
+            {option: "False", answer: true},
+        ]
     },
 
 
     {
         question: "Math.floor(x) returns x rounded down to its nearest integer",
-        answers:{
-            true: "yes",
-            false: "no",
-        },
-        correctAnswer: true,
+        answers:[
+            {option: "True", answer: true},
+            {option: "False", answer: false},
+        ]
     },
 
     {
         question: " Comparing two JavaScript objects doesn't always return false.",
-        answers:{
-            true: "yes",
-            false: "no",
-        },
-        correctAnswer: false,
+        answers:[
+            {option: "True", answer: false},
+            {option: "False", answer: true},
+        ]
+        
     },
 
     {
         question: "If not specified, Javascript counts position from 1",
-        answers:{
-            true: "yes",
-            false: "no",
-        },
-        correctAnswer: false, 
+        answers:[
+            {option: "True", answer: false},
+            {option: "False", answer: true},
+        ]
     },
 
     {
         question: " x == '5' returns true",
-        answers:{
-            true: "yes",
-            false: "no",
-        },
-        correctAnswer: true,
+        answers:[
+            {option: "True", answer: true},
+            {option: "False", answer: false},
+        ]
     },
 
     {
         question: "Given that x = 8 and y = 2, (x== 8 || y == 8) returns true",
-        answers:{
-            true: "yes",
-            false: "no",
-        },
-        correctAnswer: true, 
+        answers:[
+            {option: "True", answer: true},
+            {option: "False", answer: false},
+        ]
     },
-
-
 
 ];
 
+restartBtn.addEventListener('click', restart);
+nextBtn.addEventListener('click', next);
+submitBtn.addEventListener('click', submit);
 
 
 
-function Qgenerator(jsQuestions, Qbox, resultBox, submitBtn){
-    function showQ(jsQuestions, Qbox){
-        var output=[];
-        var answers;
-        
-        for (var i=0; i < jsQuestions.length; i++){
-            answers =[];
 
-            for (Boolean in jsQuestions[i].answers){
-                answers.push(
-                    '<label>' + '<input type="radio" name="questions'+i+'" value="'+Boolean+'">'+letter + ':'+jsQuestions[i].answers[Boolean]+'</label>'
-                );
-            }
-        
-        output.push(
-            '<div class="questions">'+jsQuestions[i].question + '</div>'+'<div class="answers">' + answers.join('')+'</div>');
-        }
-        Qbox.innerHTML=output.join('');
-    } 
+function startQuiz(){
+    var userChoice = confirm("Would you like to start the quiz?");
 
-    function showR(jsQuestions, Qbox, resultBox){
-        var answerBox = Qbox.querySelectorAll('.answers');
-
-        var userAnswer ="";
-        var numCorrect = 0;
-
-        for (var i=0; i<jsQuestions.length; i++){
-            userAnswer = (answerBox[i].querySelector('input[name=question]'+i+']:checked')||{}).value;
-
-            if(userAnswer===jsQuestions[i].correctAnswer){
-                numCorrect++
-
-                answerBox[i].style.color = 'lightblue';
-            }
-
-            else{
-                answerBox[i].style.color='red';
-            }
-        }
-        resultBox.innerHTML = numCorrect + 'out of ' + jsQuestions.length;
+    if (userChoice == "No"){
+        console.log("user don't want to be quizzed :(");
+        return;
     }
 
-    showQ(jsQuestions, Qbox);
+    console.log("user wants to be quizzed!");
+    alert("You have 30 seconds to answer 6 questions!");
+    // start timer
+    var count =30;
+    var counter =setInterval(timer, 1000);
 
-    submitBtn.onclick = function(){
-        showR(jsQuestions, Qbox, resultBox);
+    function timer(){
+        count=count-1;
+        if (count <= 0){
+           clearInterval(counter);
+           document.getElementById("quizbox").innerHTML = "Times Up!";
+           return;
+        }
+      document.getElementById("timer").innerHTML="Timer: "+ count + " secs"; 
+      }
+
+
+
+    currentQ = 0;
+    totalScore.innerHTML = myQuestions.length;
+    questionText.innerHTML = myQuestions[currentQ].question;
+
+    true.innerHTML = myQuestions[currentQ].answers[0].option;
+    true.onclick = () =>{
+        if(myQuestions[currentQ].answers[0].answer){
+            if(score<3){
+                score ++;
+                alert("you are correct!");
+            }
+        }
+        userScore.innerHTML = score; 
+        if (currentQ < 2){
+            next();
+        }
     }
+
+    false.innerHTML = myQuestions [currentQ].answers[1].option;
+    false.onclick = () =>{
+        if(myQuestions[currentQ].answers[1].answer){
+            if(score<3){
+                score ++;
+                alert("you are wrong!");
+            }
+        }
+
+        userScore.innerHTML = score; 
+        if(currentQ <2 ){
+            next();
+        }
+    }
+
 }
 
-var Qbox = document.getElementById('quiz');
-var resultBox = document.getElementById('results');
-var submitBtn = document.getElementById('submit');
 
-Qgenerator(myQuestions, Qbox, resultBox, submitBtn);
+
+startQuiz();
+
+
+
 
 
 
